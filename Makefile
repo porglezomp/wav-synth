ed4a72bfb011f5adf5c7769f9769af0ff99a2bf1
Caleb Jones <bionicle.dragon@gmail.com>
2014-09-12 21:52:34 -0400

    Use -Wall, -Werror, -pedantic
    Follow std=c99
    Use defines instead of constants

--- Makefile
+++ Makefile
@@ -1,11 +1,16 @@
 SRCS = main.c instruments.c
 OBJS = $(SRCS:.c=.o)
 EXE  = Sound.exe
+FLAGS = -std=c99 -Wall -Werror -pedantic
 
 all: $(EXE)
 
 $(EXE): $(OBJS)
-	gcc -o $(EXE) $(OBJS)
+	gcc -o $(EXE) $(OBJS) $(FLAGS)
 
 %.o: %.c
-	gcc $< -c
+	gcc $< -c $(FLAGS)
+
+clean:
+	rm $(OBJS)
+	rm $(EXE)
--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 #include <math.h>
 #include "instruments.h"
 
-const int size_of_header = 36;
-const int sample_rate = 44100;
-const int samples = sample_rate*4;
-uint8_t data[samples];
+#define SIZE_OF_HEADER 36
+#define SAMPLE_RATE 44100
+#define SECONDS 4
+#define SAMPLES SAMPLE_RATE*SECONDS
+uint8_t data[SAMPLES];
 
 void synthesize();
 void writewav();
@@ -20,8 +21,8 @@ int main()
 
 void synthesize()
 {
-  for (int i = 0; i < samples; i++) {
-    data[i] = instrument(i/(float)sample_rate)*127+127;
+  for (int i = 0; i < SAMPLES; i++) {
+    data[i] = instrument(i/(float)SAMPLE_RATE)*127+127;
   }
 }
 
@@ -53,19 +54,19 @@ void writewav()
 
   /* header*/
   fputs("RIFF", f);                              /* main chunk       */
-  write(f, 4, size_of_header + samples);         /* chunk size       */
+  write(f, 4, SIZE_OF_HEADER + SAMPLES);         /* chunk size       */
   fputs("WAVE", f);                              /* file format      */
   fputs("fmt ", f);                              /* format chunk     */
   write(f, 4, 16);                               /* size of subchunk */
   write(f, 2, 1);                                /* format (1 = PCM) */
   write(f, 2, 1);                                /* # of channels    */
-  write(f, 4, sample_rate);                      /* sample rate      */
-  write(f, 4, sample_rate);                      /* byte rate        */
+  write(f, 4, SAMPLE_RATE);                      /* sample rate      */
+  write(f, 4, SAMPLE_RATE);                      /* byte rate        */
   write(f, 2, 1);                                /* block align      */
   write(f, 2, 8);                                /* bits per sample  */
   fputs("data", f);                              /* data chunk       */
   /* body */
-  fwrite(data, 1, samples, f);                   /* actual audio     */
+  fwrite(data, 1, SAMPLES, f);                   /* actual audio     */
 
   fclose(f);
 }
